e0b1b69bfde7ef2887281f00be5a4b6cdd2488c0
rzanabusamra22 <rzanabusamra@gmail.com>
2020-11-04 11:56:49 +0200

    11:56

--- client/app.jsx
+++ client/app.jsx
@@ -1,2 +1,31 @@
+class App extends React.Component {
+    constructor(props) {
+        super(props);
 
-
+        this.state = {
+            formView: 0,
+            forms: props.props,
+            fornData: {
+                'Name': '',
+                'Email': '',
+                'Password': '',
+                'Line1': '',
+                'line2': '',
+                'City': '',
+                'State': '',
+                'ZipCode': '',
+                'Credit Card Number': '',
+                'Expiration Date': '',
+                'CCV': '',
+                'Billing Zipcode': ''
+            }
+        }
+    }
+}
+let form = {
+    1: []
+}
+ReactDOM.render(
+    <App props={forms} />,
+    document.getElementById('app')
+)
--- server.js
+++ server.js
@@ -1,2 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 8080;
 
+app.use(express.static(__dirname + '/public'));
 
+// app.get('/', (req, res) => {
+//     res.send('test')
+// })
+
+app.listen(port, (error) => {
+    if (error) { console.log('Something Wrong with server', error) }
+    else { console.log('Server is listening on port ' + port) }
+})
